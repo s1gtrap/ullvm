9abfc58b79fd9f94f1aa9936224dbf89a66fb871
William Tange <will@tan.ge>
2024-05-13 23:40:42 +0200

    Switch to tailwind

--- README.md
+++ README.md
@@ -18,4 +18,10 @@ Run the following command in the root of the project to start the Dioxus dev ser
 dx serve --hot-reload
 ```
 
+and
+
+```bash
+npx tailwindcss -i ./input.css -o ./assets/tailwind.css --watch
+```
+
 - Open the browser to http://localhost:8080
--- src/main.rs
+++ src/main.rs
@@ -37,63 +37,76 @@ fn App() -> Element {
     let mut input = use_signal(|| "".to_owned());
     let mut output = use_signal(|| "".to_owned());
     rsx! {
-        main {
-            div {
-                select {
-                    id: "example-picker",
-                    onchange: move |e: Event<FormData>| {
-                        let pick = e.data.value().parse::<usize>().unwrap() - 1;
-                        *input.write() = EXAMPLES[pick].1.to_string();
-                        let window = web_sys::window().unwrap();
-                        let document = window.document().unwrap();
-                        let element = document.get_element_by_id("example-picker").unwrap();
-                        let select: &web_sys::HtmlSelectElement = element.dyn_ref().unwrap();
-                        select.set_value("0");
-                    },
-                    option { value: "0", disabled: true, "-- example --" }
-                    for (i , (n , _)) in EXAMPLES.iter().enumerate() {
-                        option { key: "{i}", value: "{i + 1}", "{n}" }
+        main { class: "w-full bg-indigo",
+            div { class: "flex",
+                div { class: "flex-1",
+                    div { class: "flex flex-col h-screen",
+                        div { class: "flex-none",
+                            select {
+                                id: "example-picker",
+                                class: "h-12 w-full",
+                                onchange: move |e: Event<FormData>| {
+                                    let pick = e.data.value().parse::<usize>().unwrap() - 1;
+                                    *input.write() = EXAMPLES[pick].1.to_string();
+                                    let window = web_sys::window().unwrap();
+                                    let document = window.document().unwrap();
+                                    let element = document.get_element_by_id("example-picker").unwrap();
+                                    let select: &web_sys::HtmlSelectElement = element.dyn_ref().unwrap();
+                                    select.set_value("0");
+                                },
+                                option { value: "0", disabled: true, "-- example --" }
+                                for (i , (n , _)) in EXAMPLES.iter().enumerate() {
+                                    option { key: "{i}", value: "{i + 1}", "{n}" }
+                                }
+                            }
+                        }
+                        div { class: "flex-1",
+                            textarea {
+                                class: "w-full h-full font-mono",
+                                onchange: move |e: Event<FormData>| {
+                                    *input.write() = e.value().to_string();
+                                },
+                                "{input}"
+                            }
+                        }
+                        div { class: "flex-none",
+                            button {
+                                class: "h-12 w-full",
+                                onclick: move |_| {
+                                    Module::ccall(
+                                        JsValue::from_str("parse"),
+                                        JsValue::NULL,
+                                        js_sys::Array::of1(&JsValue::from_str("string")).into(),
+                                        js_sys::Array::of1(&JsValue::from_str(&input())).into(),
+                                    );
+                                    let ptr = Module::ccall(
+                                        JsValue::from_str("json"),
+                                        JsValue::from_str("number"),
+                                        js_sys::Array::new().into(),
+                                        js_sys::Array::new().into(),
+                                    );
+                                    let str = Module::UTF8ToString(ptr).as_string().unwrap();
+                                    let obj = js_sys::JSON::parse(&str).unwrap();
+                                    let out: js_sys::JsString = js_sys::JSON::stringify_with_replacer_and_space(
+                                            &obj,
+                                            &JsValue::NULL,
+                                            &JsValue::from_f64(1.0),
+                                        )
+                                        .unwrap();
+                                    tracing::info!("{}", out);
+                                    let s: String = out.into();
+                                    let m: llvm_ir::Module = serde_json::from_str(&s).unwrap();
+                                    tracing::info!("{:?}", m);
+                                    *output.write() = format!("{:#?}", m);
+                                },
+                                "Parse"
+                            }
+                        }
                     }
                 }
-                textarea {
-                    onchange: move |e: Event<FormData>| {
-                        *input.write() = e.value().to_string();
-                    },
-                    "{input}"
+                div { class: "flex-1 h-screen font-mono whitespace-pre overflow-y-scroll",
+                    div { "{output}" }
                 }
-                button {
-                    onclick: move |_| {
-                        Module::ccall(
-                            JsValue::from_str("parse"),
-                            JsValue::NULL,
-                            js_sys::Array::of1(&JsValue::from_str("string")).into(),
-                            js_sys::Array::of1(&JsValue::from_str(&input())).into(),
-                        );
-                        let ptr = Module::ccall(
-                            JsValue::from_str("json"),
-                            JsValue::from_str("number"),
-                            js_sys::Array::new().into(),
-                            js_sys::Array::new().into(),
-                        );
-                        let str = Module::UTF8ToString(ptr).as_string().unwrap();
-                        let obj = js_sys::JSON::parse(&str).unwrap();
-                        let out: js_sys::JsString = js_sys::JSON::stringify_with_replacer_and_space(
-                                &obj,
-                                &JsValue::NULL,
-                                &JsValue::from_f64(1.0),
-                            )
-                            .unwrap();
-                        tracing::info!("{}", out);
-                        let s: String = out.into();
-                        let m: llvm_ir::Module = serde_json::from_str(&s).unwrap();
-                        tracing::info!("{:?}", m);
-                        *output.write() = format!("{:#?}", m);
-                    },
-                    "Parse"
-                }
-            }
-            div { class: "output",
-                div { "{output}" }
             }
         }
     }
