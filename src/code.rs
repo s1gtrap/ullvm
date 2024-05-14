6ca022070fa5c0d901a8e6f24f3546396638b9f2
William Tange <will@tan.ge>
2024-05-14 21:28:30 +0200

    Add a border between left and right pane

--- src/code.rs
+++ src/code.rs
@@ -5,6 +5,6 @@ use dioxus::prelude::*;
 #[component]
 pub fn Code(code: String) -> Element {
     rsx! {
-        div { class: "font-mono whitespace-pre", "{code}" }
+        div { class: "font-mono whitespace-pre ml-1 bg-white box-border", "{code}" }
     }
 }
