6ed274c362ee3c47f468bde4e58806a498a1130b
William Tange <will@tan.ge>
2024-05-14 20:21:14 +0200

    Display CFG of first function

--- src/ir.rs
+++ src/ir.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
+use petgraph::graph::{DiGraph, NodeIndex};
+
 #[derive(Debug, serde::Deserialize)]
 pub struct Module {
     #[serde(rename = "FunctionList")]
-    pub funcstions: Vec<Function>,
+    pub functions: Vec<Function>,
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -14,7 +18,7 @@ pub struct Function {
     pub basic_blocks: Vec<BasicBlock>,
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Eq, Hash, PartialEq, serde::Deserialize)]
 #[serde(untagged)]
 pub enum Name {
     Name(String),
@@ -50,7 +54,7 @@ pub struct BasicBlock {
 #[derive(Debug, serde::Deserialize)]
 pub struct Operand {
     #[serde(rename = "Constant")]
-    pub consant: bool,
+    pub constant: bool,
     #[serde(rename = "Name")]
     pub name: Option<Name>,
     #[serde(rename = "Type")]
@@ -67,8 +71,33 @@ pub struct Instruction {
 
 #[derive(Debug, serde::Deserialize)]
 pub struct Terminator {
+    #[serde(rename = "Opcode")]
+    pub opcode: usize,
     #[serde(rename = "Dest")]
     pub def: Option<Name>,
     #[serde(rename = "Uses")]
     pub uses: Vec<Operand>,
+}
+
+pub fn cfg(f: &Function) -> DiGraph<&Name, ()> {
+    tracing::info!("cfg");
+    let mut g = DiGraph::new();
+    let blocks: HashMap<&'_ Name, (&'_ BasicBlock, NodeIndex)> = f
+        .basic_blocks
+        .iter()
+        .map(|b| (&b.name, (b, g.add_node(&b.name))))
+        .collect();
+    for b in &f.basic_blocks {
+        match (b.term.opcode, &b.term.uses[..]) {
+            (2, &[ref l]) => {
+                g.add_edge(blocks[&b.name].1, blocks[&l.name.as_ref().unwrap()].1, ());
+            }
+            (2, &[_, ref l, ref r]) => {
+                g.add_edge(blocks[&b.name].1, blocks[&l.name.as_ref().unwrap()].1, ());
+                g.add_edge(blocks[&b.name].1, blocks[&r.name.as_ref().unwrap()].1, ());
+            }
+            _ => tracing::warn!("not yet implemented: {:?}", b.term),
+        }
+    }
+    g
 }
