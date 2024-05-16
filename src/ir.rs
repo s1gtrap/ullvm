bc174657bea7c4827a68dfddac62d5cd8d271a12
William Tange <will@tan.ge>
2024-05-16 16:27:41 +0200

    Always run 10 iterations as a placeholder for now

--- src/ir.rs
+++ src/ir.rs
@@ -114,64 +114,80 @@ pub fn cfg(f: &Function) -> DiGraph<&Name, ()> {
 
 pub fn lva(f: &Function) -> Vec<(HashSet<&Name>, HashSet<&Name>)> {
     tracing::info!("lva");
-    let (_, block_indices) = f
-        .basic_blocks
-        .iter()
-        .fold((0, HashMap::new()), |(l, mut m), b| {
-            m.insert(l, b);
-            (l + b.insts.len() + 1, m)
-        });
+    let (_, block_indices) =
+        f.basic_blocks
+            .iter()
+            .fold((f.params.len(), HashMap::new()), |(l, mut m), b| {
+                m.insert(l, b);
+                (l + b.insts.len() + 1, m)
+            });
     tracing::info!("{block_indices:#?}");
     let mut lives = vec![
         (HashSet::new(), HashSet::new());
         f.basic_blocks.iter().map(|b| b.insts.len() + 1).sum()
     ];
-    for (i, (ref mut r#in, ref mut out)) in lives.iter_mut().enumerate().rev() {
-        tracing::info!("i = {i}");
-        let (block_idx, block) = block_indices
-            .iter()
-            .filter(|&(j, _)| *j <= i)
-            .max_by_key(|&(j, _)| *j)
-            .unwrap();
-        tracing::info!("{block_idx}");
-        if let Some(inst) = &block.insts.get(i - block_idx) {
-            tracing::info!(" insts[{}] = {:?}", i - block_idx, inst);
-            let def = &block.insts[i - block_idx].def;
-            let def: HashSet<_> = def.iter().collect();
-            let r#use: HashSet<_> = block.insts[i - block_idx]
-                .uses
+    for bb in &f.basic_blocks {
+        tracing::info!("{}", bb.insts.len());
+    }
+    for _ in 0..10 {
+        for (i, (ref mut r#in, ref mut out)) in lives.iter_mut().enumerate().rev() {
+            let i = i + f.params.len();
+            tracing::info!("i = {i}");
+            let (block_idx, block) = block_indices
                 .iter()
-                .filter_map(|o| {
-                    if !o.constant {
-                        Some(o.name.as_ref().unwrap())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            tracing::info!("use[{i}] = {:?}", r#use);
-            *r#in = r#use.union(&(&*out - &def)).cloned().collect();
-            tracing::info!(" in[{i}] = {:?}", r#use);
-        } else {
-            tracing::info!(" term = {:?}", &block.term);
-            let def = &block.term.def;
-            let def: HashSet<_> = def.iter().collect();
-            let r#use: HashSet<_> = block
-                .term
-                .uses
-                .iter()
-                .filter_map(|o| {
-                    if !o.constant {
-                        Some(o.name.as_ref().unwrap())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            tracing::info!("use[{i}] = {:?}", r#use);
-            *r#in = r#use.union(&(&*out - &def)).cloned().collect();
-            tracing::info!(" in[{i}] = {:?}", r#use);
+                .filter(|&(j, _)| *j <= i)
+                .max_by_key(|&(j, _)| *j)
+                .unwrap();
+            tracing::info!("{block_idx}");
+
+            // in[i] = use[i] U (out[i] - def[i])
+            if let Some(inst) = &block.insts.get(i - (block_idx)) {
+                tracing::info!(" insts[{}] = {:?}", i - block_idx, inst);
+                let def = &block.insts[i - block_idx].def;
+                let def: HashSet<_> = def.iter().collect();
+                let r#use: HashSet<_> = block.insts[i - block_idx]
+                    .uses
+                    .iter()
+                    .filter_map(|o| {
+                        if !o.constant {
+                            Some(o.name.as_ref().unwrap())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+                tracing::info!("use[{i}] = {:?}", r#use);
+                *r#in = r#use.union(&(&*out - &def)).cloned().collect();
+                tracing::info!(" in[{i}] = {:?}", r#use);
+            } else {
+                tracing::info!(" term = {:?}", &block.term);
+                let def = &block.term.def;
+                let def: HashSet<_> = def.iter().collect();
+                let r#use: HashSet<_> = block
+                    .term
+                    .uses
+                    .iter()
+                    .filter_map(|o| {
+                        if !o.constant {
+                            Some(o.name.as_ref().unwrap())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+                tracing::info!("use[{i}] = {:?}", r#use);
+                *r#in = r#use.union(&(&*out - &def)).cloned().collect();
+                tracing::info!(" in[{i}] = {:?}", r#use);
+            }
+
+            // out[i] = U_s=succ[i] (in[s] U phis[s])
+            if let Some(inst) = &block.insts.get(i - (block_idx) + 1) {
+                // all insts only have one subsequent successor
+            } else {
+                // terminators must be looked up in the cfg
+            }
         }
+        tracing::info!("lives = {:?}", lives);
     }
     lives
 }
@@ -179,6 +195,8 @@ pub fn lva(f: &Function) -> Vec<(HashSet<&Name>, HashSet<&Name>)> {
 #[test]
 fn test_lva() {
     tracing_subscriber::fmt::init();
+
+    // min.ll
     assert_eq!(
         lva(&Function {
             name: "main".to_string(),
@@ -219,5 +237,255 @@ fn test_lva() {
             HashSet::from([&Name::Name("argc".to_string())]),
             HashSet::new(),
         )],
+    );
+    // for1.ll
+    assert_eq!(
+        lva(&Function {
+            name: "main".to_string(),
+            params: vec![
+                Param {
+                    name: Name::Number(0),
+                    ty: Type {
+                        id: 13,
+                        name: "i32".to_string(),
+                    },
+                },
+                Param {
+                    name: Name::Number(1),
+                    ty: Type {
+                        id: 15,
+                        name: "ptr".to_string(),
+                    },
+                },
+            ],
+            basic_blocks: vec![
+                BasicBlock {
+                    name: Name::Number(2),
+                    insts: vec![
+                        Instruction {
+                            def: Some(Name::Number(3)),
+                            uses: vec![
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(0)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: true,
+                                    name: None,
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                            ],
+                        },
+                        Instruction {
+                            def: None,
+                            uses: vec![Operand {
+                                constant: false,
+                                name: Some(Name::Number(3)),
+                                ty: Type {
+                                    id: 13,
+                                    name: "i1".to_string(),
+                                },
+                            },],
+                        },
+                    ],
+                    term: Terminator {
+                        opcode: 2,
+                        def: None,
+                        uses: vec![Operand {
+                            constant: false,
+                            name: Some(Name::Number(3)),
+                            ty: Type {
+                                id: 13,
+                                name: "i1".to_string(),
+                            },
+                        },],
+                    },
+                },
+                BasicBlock {
+                    name: Name::Number(4),
+                    insts: vec![Instruction {
+                        def: None,
+                        uses: vec![Operand {
+                            constant: true,
+                            name: None,
+                            ty: Type {
+                                id: 13,
+                                name: "i32".to_string(),
+                            },
+                        }],
+                    }],
+                    term: Terminator {
+                        opcode: 1,
+                        def: None,
+                        uses: vec![Operand {
+                            constant: true,
+                            name: None,
+                            ty: Type {
+                                id: 13,
+                                name: "i32".to_string(),
+                            },
+                        }],
+                    },
+                },
+                BasicBlock {
+                    name: Name::Number(5),
+                    insts: vec![
+                        Instruction {
+                            def: Some(Name::Number(6)),
+                            uses: vec![
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(8)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: true,
+                                    name: None,
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                            ],
+                        },
+                        Instruction {
+                            def: Some(Name::Number(7)),
+                            uses: vec![
+                                Operand {
+                                    constant: true,
+                                    name: None,
+                                    ty: Type {
+                                        id: 15,
+                                        name: "ptr".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(6)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: true,
+                                    name: None,
+                                    ty: Type {
+                                        id: 15,
+                                        name: "ptr".to_string(),
+                                    },
+                                },
+                            ],
+                        },
+                        Instruction {
+                            def: Some(Name::Number(8)),
+                            uses: vec![
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(6)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: true,
+                                    name: None,
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                            ],
+                        },
+                        Instruction {
+                            def: Some(Name::Number(9)),
+                            uses: vec![
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(8)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                                Operand {
+                                    constant: false,
+                                    name: Some(Name::Number(0)),
+                                    ty: Type {
+                                        id: 13,
+                                        name: "i32".to_string(),
+                                    },
+                                },
+                            ],
+                        },
+                        Instruction {
+                            def: None,
+                            uses: vec![Operand {
+                                constant: false,
+                                name: Some(Name::Number(9)),
+                                ty: Type {
+                                    id: 13,
+                                    name: "i1".to_string(),
+                                },
+                            },],
+                        },
+                    ],
+                    term: Terminator {
+                        opcode: 2,
+                        def: None,
+                        uses: vec![Operand {
+                            constant: false,
+                            name: Some(Name::Number(9)),
+                            ty: Type {
+                                id: 13,
+                                name: "i1".to_string(),
+                            },
+                        },],
+                    },
+                },
+            ],
+        }),
+        vec![
+            (
+                HashSet::from([&Name::Number(0)]),
+                HashSet::from([&Name::Number(0), &Name::Number(3)]),
+            ),
+            (
+                HashSet::from([&Name::Number(0), &Name::Number(3)]),
+                HashSet::from([&Name::Number(0)]),
+            ),
+            (HashSet::from([]), HashSet::from([])),
+            (
+                HashSet::from([&Name::Number(0)]),
+                HashSet::from([&Name::Number(0), &Name::Number(6)]),
+            ),
+            (
+                HashSet::from([&Name::Number(0), &Name::Number(6)]),
+                HashSet::from([&Name::Number(0), &Name::Number(6)]),
+            ),
+            (
+                HashSet::from([&Name::Number(0), &Name::Number(6)]),
+                HashSet::from([&Name::Number(0), &Name::Number(8)]),
+            ),
+            (
+                HashSet::from([&Name::Number(0), &Name::Number(8)]),
+                HashSet::from([&Name::Number(8), &Name::Number(9)]),
+            ),
+            (
+                HashSet::from([&Name::Number(8), &Name::Number(9)]),
+                HashSet::from([&Name::Number(8)]),
+            ),
+        ],
     );
 }
