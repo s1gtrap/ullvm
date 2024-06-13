d9f8ed09d133556035a0e89e3870e659c4c0950b
William Tange <will@tan.ge>
2024-06-13 19:07:33 +0200

    Interference doesn't depend on type of instruction

--- src/interf.rs
+++ src/interf.rs
@@ -1,6 +1,5 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
-use either::Either;
 use petgraph::graph::UnGraph;
 
 use crate::ir::{self, Function, Lva2, Name};
@@ -9,21 +8,16 @@ pub fn interf(f: &Function, lva: Lva2) -> UnGraph<Name, ()> {
     let def = ir::def(f);
     let mut g = UnGraph::new_undirected();
     let mut ids = HashMap::new();
-    for (idx, (r#in, out, inst)) in lva.iter().enumerate() {
-        match inst {
-            Either::Left(i) => {
-                for &def in &def[idx] {
-                    let node1 = *ids.entry(def).or_insert_with(|| g.add_node(def.clone()));
+    for (idx, (_in, out, _inst)) in lva.iter().enumerate() {
+        for &def in &def[idx] {
+            let node1 = *ids.entry(def).or_insert_with(|| g.add_node(def.clone()));
 
-                    for out in out {
-                        let node2 = *ids.entry(out).or_insert_with(|| g.add_node(out.clone()));
-                        if node1 != node2 {
-                            g.add_edge(node1, node2, ());
-                        }
-                    }
+            for out in out {
+                let node2 = *ids.entry(out).or_insert_with(|| g.add_node(out.clone()));
+                if node1 != node2 {
+                    g.add_edge(node1, node2, ());
                 }
             }
-            Either::Right(t) => {}
         }
     }
     g
