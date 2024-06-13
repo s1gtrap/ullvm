f8282bdaf34b8b0a533bc6079dc8faad04250501
William Tange <will@tan.ge>
2024-06-13 18:35:23 +0200

    Begin work on interference

--- src/interf.rs
+++ src/interf.rs
@@ -1,16 +1,28 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use either::Either;
 use petgraph::graph::UnGraph;
 
-use crate::ir::{Lva2, Name};
+use crate::ir::{self, Function, Lva2, Name};
 
-pub fn interf(lva: Lva2) -> UnGraph<Name, ()> {
+pub fn interf(f: &Function, lva: Lva2) -> UnGraph<Name, ()> {
+    let def = ir::def(f);
     let mut g = UnGraph::new_undirected();
-    for (r#in, out, inst) in lva {
+    let mut ids = HashMap::new();
+    for (idx, (r#in, out, inst)) in lva.iter().enumerate() {
         match inst {
-            Either::Left(_) => {}
-            Either::Right(_) => {}
+            Either::Left(i) => {
+                for &def in &def[idx] {
+                    let node1 = g.add_node(def.clone());
+                    ids.insert(def, node1);
+
+                    for out in out {
+                        let node2 = g.add_node(out.clone());
+                        ids.insert(out, node2);
+                    }
+                }
+            }
+            Either::Right(t) => {}
         }
     }
     g
